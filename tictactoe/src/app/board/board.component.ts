2e2d8c66a15180ca45de6ef4bd0ca4302dcc3359
StroschneiderFabian <stro.fabian@gmail.com>
2024-10-31 13:49:17 +0100

    feat: determines the winner and can restart the game

--- tictactoe/src/app/board/board.component.ts
+++ tictactoe/src/app/board/board.component.ts
@@ -1,6 +1,8 @@
-import {Component, signal, WritableSignal, Signal, viewChild} from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
 import {NgClass} from '@angular/common';
 import {SquaresComponent} from './squares/squares.component';
+import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatButtonToggle} from '@angular/material/button-toggle';
 
 
 @Component({
@@ -8,7 +10,10 @@ import {SquaresComponent} from './squares/squares.component';
   standalone: true,
   imports: [
     NgClass,
-    SquaresComponent
+    SquaresComponent,
+    MatCard,
+    MatCardContent,
+    MatButtonToggle,
   ],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
@@ -16,12 +21,33 @@ import {SquaresComponent} from './squares/squares.component';
 export class BoardComponent {
   protected board: WritableSignal<(Player | null)[]> = signal(Array(9).fill(null));
   protected currentPlayer: Player = Player.X;
+  protected isWinner: boolean = false;
 
   protected makeMove(index: number): void {
     if (!this.board()[index]) {
       this.board()[index] = this.currentPlayer;
-      this.currentPlayer = this.currentPlayer === Player.X ? Player.O : Player.X;
+      this.isWinner = this.checkWinner();
+      if (!this.isWinner) {
+        this.currentPlayer = this.currentPlayer === Player.X ? Player.O : Player.X;
+      }
     }
+  }
+
+  private checkWinner(): boolean {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+    return winningCombinations.some(combination =>
+      combination.every(index => this.board()[index] === this.currentPlayer)
+    );
+  }
+
+  protected restart(): void {
+    this.board.set(Array(9).fill(null));
+    this.currentPlayer = Player.X;
+    this.isWinner = false;
   }
 }
 
